4d5b517037587db82b166f8a24b55bfd708d4e92
karaloabshuk <karaloabshuk@gmail.com>
2026-08-05 09:56:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log('spurs')
+
+var k=14
+
+console.log(k)
+
+let num1
+
+let num2
+
+num1=2
+
+num2=4
+
+console.log(num1>7)
+
+console.log("karalo"+" "+"asmamaw")
+
+function ferran (){
+    console.log("abebe")
+    console.log("abebe")
+    console.log("abebe")
+}
+
+ferran ()
+
+function adder(x,y)
+{
+    var c = x+y
+    console.log(k)
+    return c
+}
+
+var added=adder(5,7)
+console.log(added)
+
+function average (x,y)
+{
+var avg
+avg=adder(x,y)/2
+}
+
+if (1==1){
+    console.log("abebe beso bela")
+}
+
+var score=66
+var pass=50
+
+
+if (score > pass){
+    console.log("passed")
+}else{
+    console.log("failed")
+}
