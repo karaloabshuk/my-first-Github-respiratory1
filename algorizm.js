6f7405a34f94445096b8a1cfd6dac0947c7015e0
karaloabshuk <karaloabshuk@gmail.com>
2026-08-06 15:36:45 +0300

    Update algorizm.js

--- algorizm.js
+++ algorizm.js
@@ -376,3 +376,14 @@ function binarySearch(arr, target) {
     return -1
 
 
+    function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    }
+    return arr
+
+
