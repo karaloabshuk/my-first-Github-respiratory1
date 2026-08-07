0d3b9557790780f72d5460437dff544e7e0892bf
karaloabshuk <karaloabshuk@gmail.com>
2026-08-07 16:25:34 +0300

    Update algorizm.js

--- algorizm.js
+++ algorizm.js
@@ -1,3 +1,19 @@
+function binarySearch(arr, target) {
+  let left = 0, right = arr.length - 1
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    if (arr[mid] === target) return mid
+    else if (arr[mid] < target) left = mid + 1
+    else right = mid - 1
+  }
+  return -1 // not found
+}
+
+binarySearch([1, 2, 3, 5, 8], 5) // index 3
+
+
+
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
